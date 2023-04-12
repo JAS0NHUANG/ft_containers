9f517c4576d01ec5f13e39fc44f927d95393b128
Jas0nhuang <49294117+JAS0NHUANG@users.noreply.github.com>
2023-04-12 10:03:36 +0200

    vector underway

--- incs/vector.hpp
+++ incs/vector.hpp
@@ -8,9 +8,10 @@ namespace ft {
 template< class T, class Allocator = std::allocator<T> >
 class vector {
 public:
-/****************/
-/* Member types */
-/****************/
+/************************/
+/* Member types         */
+/* @brief some typedefs */
+/************************/
 	// basics
 	typedef				T										value_type;
 	typedef				Allocator								allocator_type;
@@ -34,7 +35,7 @@ public:
 	// default
 	explicit vector(const allocator_type& alloc = allocator_type());
 
-/*
+	/*
 	// fill
 	explicit vector(size_type n, const value_type& val = value_type(),
 		const allocator_type& alloc = allocator_type());
@@ -51,29 +52,21 @@ public:
 	//destructor
 	~vector(void);
 
-/** Capacity related member functions
- *
- * @brief	max_size	return the max_size given by allocator
- * @brief	reserve		reserve a certain memory spaces
- * @brief	capacity	reserve a certain memory spaces
- *
- */
-
-	size_type	max_size(void) const {
-		return ( this->_allocator.max_size() );
-	};
-	size_type	size(void) const {
-		return ( this->_allocator.max_size() );
-	};
-	void		reserve(size_type n) {
-		(void)n;
-
-	};
-
-	size_type	capacity(void) const {
-		return ( 5 );
-	};
-
+/***************************************************************/
+/* Capacity related member functions                           */
+/* @brief	size		return the current vector size         */
+/* @brief	max_size	return the max size given by allocator */
+/* @brief	size		change the vector size                 */
+/* @brief	capacity	return size of allocated capacity      */
+/* @brief	empty       check whether vector is empty          */
+/* @brief	reserve		reserve a certain memory spaces        */
+/***************************************************************/
+	size_type	size(void) const;
+	size_type	max_size(void) const;
+	void		resize(size_type n, value_type val = value_type());
+	size_type	capacity(void) const;
+	bool		empty(void) const;
+	void		reserve(size_type n);
 
 private:
 	allocator_type	_allocator;
--- incs/vector.tpp
+++ incs/vector.tpp
@@ -1,30 +1,58 @@
-/**
- * Consructor
- */
 
 namespace ft {
-// default
-template<class T, class A>
-ft::vector<T, A>::vector( const allocator_type& alloc):
-	_allocator(alloc), _size(0)
+
+	/***************************/
+	/* Consructor & destructor */
+	/***************************/
+	// default
+	template<class T, class A>
+	ft::vector<T, A>::vector( const allocator_type& alloc):
+		_allocator(alloc), _size(0) { }
+
+	/*
+	// fill
+	explicit vector(size_type n, const value_type& val = value_type(),
+		const allocator_type& alloc = allocator_type());
+
+	// range
+	template <class InputIterator>
+	vector (InputIterator first, InputIterator last, \
+		const allocator_type& alloc = allocator_type());
+
+	// copy
+	vector (const vector& x);
+	*/
+
+	//destructor
+	template<class T, class A>
+	ft::vector<T, A>::~vector(void)
 	{ }
-/*
-// fill
-explicit vector(size_type n, const value_type& val = value_type(),
-	const allocator_type& alloc = allocator_type());
 
-// range
-template <class InputIterator>
-vector (InputIterator first, InputIterator last, \
-	const allocator_type& alloc = allocator_type());
+	/*************************************/
+	/* Capacity related member functions */
+	/*************************************/
+	template<class T, class A>
+	typename ft::vector<T, A>::size_type ft::vector<T, A>::max_size(void) const
+	{
+		return ( this->_allocator.max_size() );
+	}
 
-// copy
-vector (const vector& x);
-*/
+	template<class T, class A>
+	typename ft::vector<T, A>::size_type ft::vector<T, A>::size(void) const
+	{
+		return ( this->_allocator.max_size() );
+	}
 
-//destructor
-template<class T, class A>
-ft::vector<T, A>::~vector(void)
-{ }
+	template<class T, class A>
+	void ft::vector<T, A>::reserve(size_type n) 
+	{
+		(void)n;
+	}
+
+	template<class T, class A>
+	typename ft::vector<T, A>::size_type ft::vector<T, A>::capacity(void) const
+	{
+		return ( 5 );
+	};
 
 } // namespace ft
